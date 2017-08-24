2ced7537c175b65cce75538326fdb24bcdec260f
ashzak990@gmail.com <ashzak990@gmail.com>
2017-08-24 17:28:28 +0300

    class employee

--- employees.py
+++ employees.py
@@ -2,8 +2,9 @@ from people import People
 
 
 class Employees(People):
-    def __init__(self, name, patronymic, surname, birthday, phone_number, id_employee):
-        super().__init__(name, patronymic, surname, birthday, phone_number)
+    def __init__(self, id_employee):
+        # super().__init__(name, patronymic, surname, birthday, phone_number)
+        super(People, self).__init__()
         self._id_emp = id_employee
 
     def __str__(self):
@@ -12,6 +13,7 @@ class Employees(People):
 
 
 if __name__ == '__main__':
-    newbie = Employees('Пертр', 'Петрович', 'Петров', '31.08.1989', 89284453641, '123')
+    ppl = People('Пертр', 'Петрович', 'Петров', '31.08.1989', 89284453641)
+    newbie = Employees('123')
     print(newbie.patronymic)
 
