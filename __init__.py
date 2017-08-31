a1b9176b58c07f6eecf43848fd64b96b0735bc2c
ashzak990@gmail.com <ashzak990@gmail.com>
2017-08-31 17:16:54 +0300

    rewrited enterprise

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from enterprise import Enterpise, People, Employees
+from enterprise import Enterpise, People, Employee
 
 
 
-ark = Enterpise(1, 'ARK Group', 'some address', 12345678912, 'some@email', 89281546474)
+ark = Enterpise('ARK Group', 'some address', 12345678912, 'some@email', 89281546474)
 ppl = People('Петр', 'Петрович', 'Петров', '31.08.1989', 89284453641)
-emp = Employees('Петр', 'Петрович', 'Петров', '31.08.1989', 89284453641, 1, 2, 10000)
+emp = Employee(1, 2, 10000)
 
 print('Enterprise:', ark, 'People:', ppl, 'Employee:', emp)
