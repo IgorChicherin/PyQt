ac77b131fc24f3c5ee1500ba95f3f9a3d488e93b
ashzak990@gmail.com <ashzak990@gmail.com>
2017-08-31 11:17:39 +0300

    orm

--- people.py
+++ people.py
@@ -1,67 +1,40 @@
-class People:
+from datetime import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///:memory:', echo=False)
+Base = declarative_base(bind=engine)
+
+class People(Base):
+    __tablename__ = 'employees'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    patronymic = Column(String)
+    surname = Column(String)
+    birthday = Column(Date)
+    phone_number = Column(Integer)
+
+
     def __init__(self, name, patronymic, surname, birthday, phone_number):
+        Base.metadata.create_all(engine)
         if self.__check(name, 'name'):
-            self._name = name
+            self.name = name
         if self.__check(patronymic, 'patronymic'):
-            self._patronymic = patronymic
+            self.patronymic = patronymic
         if self.__check(surname, 'surname'):
-            self._surname = surname
+            self.surname = surname
         if self.__check(birthday, 'birthday'):
-            self._birthday = birthday
+            self.birthday = datetime.strptime(birthday, '%d.%m.%Y')
         if self.__check(phone_number, 'phone'):
-            self._phone_number = phone_number
+            self.phone_number = phone_number
 
-    def __str__(self):
-        return 'Фамилия: %s \n' \
-               'Имя: %s \n' \
-               'Отчество: %s \n' \
-               'Год рождения: %s \n' \
-               'Телефон: %s' % (self.surname, self.name, self.patronymic, self.birthday, self.phone_number)
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if self.__check(value, 'name'):
-            self._name = value
-
-    @property
-    def patronymic(self):
-        return self._patronymic
-
-    @patronymic.setter
-    def patronymic(self, value):
-        if self.__check(value, 'patronymic'):
-            self._patronymic = value
-
-    @property
-    def surname(self):
-        return self._surname
-
-    @surname.setter
-    def surname(self, value):
-        if self.__check(value, 'surname'):
-            self._surname = value
-
-    @property
-    def birthday(self):
-        return self._birthday
-
-    @surname.setter
-    def surname(self, value):
-        if self.__check(value, 'birthday'):
-            self._birthday = value
-
-    @property
-    def phone_number(self):
-        return self._phone_number
-
-    @phone_number.setter
-    def phone_number(self, value):
-        if self.__check(value, 'phone'):
-            self._phone_number = value
+    def __repr__(self):
+        return '<People surname=%s  name=%s patronymic=%s birthday=%s ' \
+               'phone_number=%s>' % (self.surname, self.name, self.patronymic, self.birthday, self.phone_number)
 
     def __check(self, value, value_type):
         '''
@@ -88,4 +61,9 @@ class People:
 
 if __name__ == '__main__':
     emp = People('Петр', 'Петрович', 'Петров', '31.08.1989', 89284453641)
-    print(emp)
+    Sesson = sessionmaker(bind=engine)
+    session = Sesson()
+    session.add(emp)
+    session.commit()
+    res = session.query(People).filter(People.name=='Петр').first()
+    print(res)
