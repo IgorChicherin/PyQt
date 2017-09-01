8a99cb3e759af2c4c92915fdff5b07c850648981
ashzak990@gmail.com <ashzak990@gmail.com>
2017-09-01 16:11:20 +0300

    SQLAlchemy, Peewee

--- __init__.py
+++ __init__.py
@@ -1,9 +1,33 @@
 from enterprise import Enterpise, People, Employee
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+from peewee_library import Author, Book, Journal, BookAuthor, JournalAuthor, db
 
-ark = Enterpise('ARK Group', 'some address', 12345678912, 'some@email', 89281546474)
+# SQLAlchemy
+engine = create_engine('sqlite:///enterprise.db', echo=True)
+Base = declarative_base()
+
+Base.metadata.create_all(engine)
+Sesson = sessionmaker(bind=engine)
+session = Sesson()
+ark = Enterpise(name='ARK Group',
+                adress='some address',
+                inn=12345678912,
+                email='some@email',
+                phone_number=89281546474)
 ppl = People('Петр', 'Петрович', 'Петров', '31.08.1989', 89284453641)
-emp = Employee(1, 2, 10000)
+emp = Employee(1, 1, 1000)
+session.add_all([ark, ppl, emp])
+session.commit()
 
-print('Enterprise:', ark, 'People:', ppl, 'Employee:', emp)
+# Peewee
+db.create_tables([Author, Book, Journal, BookAuthor, JournalAuthor])
+Author().update_author('qweqqe', 1)
+Journal().delete_journal(1)
+Journal().add_journal('Magazine', 'weqw', '30.08.1922', 1, 'azxcz', 1)
+print(Book().search_book('qwe'))
+a = Author(name='asda')
+a.save()
